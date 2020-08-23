27f9fd37f6ed54b79f92f2502f7ff850250df1e1
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-24 00:57:36 +0700

    save items to cart section

--- src/components/_base/Cart.vue
+++ src/components/_base/Cart.vue
@@ -1,7 +1,7 @@
 <template>
   <aside class="cart-section col-md-4">
     <div class="cart-contents" id="cart-section">
-      <div class="empty-cart">
+      <div class="empty-cart" v-if="count.length < 1">
         <img src="../../assets/food-and-restaurant.png">
         <h5>Your cart is empty</h5>
         <span>Please add some items from the menu</span>
@@ -9,6 +9,52 @@
           <button class="btn btn-outline-info" onclick="window.history.go(-1)">Back</button>
         </div>
       </div>
+      <div v-else>
+        <div class="a" v-for="(item, index) in count" :key="index">
+          <div class="cart-item">
+            <div class="item-image">
+              <img :src="require(`../../${ item.product_image }`)">
+            </div>
+            <div class="item-detail">
+              <div class="item-name">
+                <strong>{{ item.product_name }}</strong>
+              </div>
+              <div class="item-calculation">
+                <div class="input-group">
+                  <button type="button" class="btn btn-qty-min"><span>&minus;</span></button>
+                  <input type="text" name="" class="form-control qty" v-model="item.qty" min="1" max="100">
+                  <button type="button" class="btn btn-qty-min"><span>&plus;</span></button>
+                </div>
+                <div class="item-price"><span>Rp {{ item.subtotal }}</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cart-footer">
+          <div class="cart-total">
+            <span>Total:</span>
+            <span class="float-right">Rp. 105.000*</span>
+          </div>
+          <span>*Belum termasuk ppn</span>
+          <div class="button-section">
+            <a href="#" class="btn my-primary" data-toggle="modal" data-target="#checkoutmodal">Checkout</a>
+            <a href="#" class="btn my-danger">Cancel</a>
+          </div>
+        </div>
+      </div>
     </div>
   </aside>
 </template>
+<script>
+export default {
+  name: 'Cart',
+  props: ['count'],
+  data() {
+    return {
+      cartFilled: false
+    }
+  },
+  methods: {}
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <Navbar />
+    <Navbar :count="cartCount" />
     <section class="contents">
       <b-row>
         <Sidebar />
-        <Cards />
-        <Cart />
+        <div class="main-content list-menu col-md-7 col-12">
+          <div class="container">
+            <div class="row">
+              <b-card class="collection-item" v-for="(item, index) in products" :key="index">
+                <img v-bind:src="require(`../${item.product_image}`)" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="product-name">{{ item.product_name }}</h5>
+                  <h5 class="price">Rp {{item.product_price}}</h5>
+                  <b-button class="my-primary col-12" @click="addToCart(item)">Add</b-button>
+                </div>
+              </b-card>
+              <!-- <div class="col-md-4" v-for="(item, index) in products" :key="index">
+                <b-card title="Card Title" v-bind:img-src="require('../assets/menu/cappucino.jpg')" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+                  <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </b-card-text>
+                  <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+              </div> -->
+            </div>
+          </div>
+        </div>
+        <Cart :count="cart" />
+        <ModalItem />
       </b-row>
     </section>
   </div>
@@ -14,16 +36,66 @@
 // @ is an alias to /src
 import Navbar from '../components/_base/Navbar'
 import Sidebar from '../components/_base/Sidebar'
-import Cards from '../components/_base/Cards'
 import Cart from '../components/_base/Cart'
-
+import ModalItem from '../components/_base/Modal_form'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     Navbar,
     Sidebar,
-    Cards,
-    Cart
+    Cart,
+    ModalItem
+  },
+  data() {
+    return {
+      cartCount: 0,
+      cart: [],
+      page: 1,
+      limit: 9,
+      sort: '',
+      products: [],
+      base_url: process.env.VUE_APP_BASE_URL,
+      alert: false,
+      isMsg: 'Tes',
+      isUpdate: false,
+      product_id: ''
+    }
+  },
+  created() {
+    this.get_product()
+    console.log(this.cart)
+  },
+  methods: {
+    addToCart(data) {
+      const cekIndex = this.cart.findIndex(obj => obj.product_id === data.product_id)
+      // console.log(cekIndex)
+      if (cekIndex >= 0) {
+        this.cart[cekIndex].qty += 1
+        this.cart[cekIndex].subtotal += this.cart[cekIndex].subtotal
+        this.cartCount += 1
+      } else {
+        const setCart = {
+          product_id: data.product_id,
+          product_name: data.product_name,
+          product_image: data.product_image,
+          qty: 1,
+          subtotal: data.product_price
+        }
+        this.cart.push(setCart)
+        this.cartCount += 1
+      }
+      console.log(this.cart)
+    },
+    get_product() {
+      axios.get(`http://127.0.0.1:3000/product?page=${this.page}&limit=${this.limit}`)
+        .then(res => {
+          this.products = res.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 
