aa6550dadb6cb829544e9a152465d9fcefc3af7b
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-23 08:16:38 +0700

    add modal

---
+++ src/components/_base/Modal_form.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Add Item Modal -->
+  <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Modal_add_product',
+  data() {
+    return {
+      alert: true,
+      isMsg: 'Tes',
+      form: {
+        product_name: '',
+        product_price: '',
+        product_image: '',
+        category_id: '',
+        product_status: ''
+      }
+    }
+  }
+}
+
+</script>
--- src/components/_base/Sidebar.vue
+++ src/components/_base/Sidebar.vue
@@ -3,7 +3,7 @@
     <ul>
       <li><a><img src="../../assets/fork.png"></a></li>
       <li><a><img src="../../assets/clipboard.png"></a></li>
-      <li><a href="#add-item" data-toggle="modal" data-target="#add-item-modal"><img src="../../assets/add.png"></a></li>
+      <li><a href="#add-item" v-b-modal.modal-1><img src="../../assets/add.png"></a></li>
     </ul>
   </b-col>
 </template>
