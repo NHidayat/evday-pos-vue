a7f08d98c4dc51fc5b428e2e3a20a74b4a22be8c
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-25 08:47:40 +0700

    add full navbar

---
+++ src/components/_base/Navbar_full.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav>
+      <b-container fluid>
+        <b-row>
+          <div class="navigation col-md-12">
+            <b-row>
+              <b-col cols="1" md="1" class="menu-icon-section">
+                <img src="../../assets/menu.png" class="menu-icon">
+              </b-col>
+              <b-col md="10" cols="10" class="nav-title">
+                <h4 class="text-center">{{ title }}</h4>
+              </b-col>
+              <b-col cols="1" md="1" class="search-section">
+                <img src="../../assets/search.png" class="search-icon">
+              </b-col>
+            </b-row>
+          </div>
+        </b-row>
+      </b-container>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+    }
+  },
+  props: ['title'],
+  methods: {
+  }
+}
+
+</script>
